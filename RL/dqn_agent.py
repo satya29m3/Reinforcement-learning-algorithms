714e0a69e318e22fe5539aa53f80d744622af816
satya29m3 <46390390+satya29m3@users.noreply.github.com>
2020-01-30 21:20:46 +0530

    Delete dqn_agent.py

--- RL/dqn_agent.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-from keras.callbacks import Tensorboard
-class DQNagent:
-    def create_model(self):
-        model = tf.keras.model.Sequential([
-            tf.keras.layers.Conv2D()
-        ]) 
